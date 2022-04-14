17f122c61fb6ce8c4699422739f8265aaa17143b
John Pohl <johnpohl34@gmail.com>
2022-04-14 18:06:42 -0400

    added webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,12 +18,23 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin'
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          
+        }
       ],
     },
   };
