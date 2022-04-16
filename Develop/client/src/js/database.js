2af3aeb0e8bc17ad3234843bd0c78827328d6d43
John Pohl <johnpohl34@gmail.com>
2022-04-16 14:41:05 -0400

    database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, value) => {
+  console.log('Hit request to db')
+  // new connection to DB
+  const jateDB = await openDB('jate', 1);
+  // new transaction for DB and privalleges
+  const tx = jateDB.transaction('jate', 'readwrite');
+  // open specific store
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, Value: content });
+  // confirm results
+  const result = await request;
+  console.log('data saved!', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+// new DB connection and version
+const jateDB = await openDB('jate', 1);
+
+const tx= jateDB.transaction('jate', 'readonly');
+
+const store = tx.objectStore('jate');
+// get all DB data
+const request = store.getAll();
+// get results
+const result = await request;
+console.log('result.value', result);
 
 initdb();
